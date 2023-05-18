7f48151e96cb2ca88270b1103afe4c119c044e10
José Lizardo <jose.212002lizardo@gmail.com>
2023-05-18 11:09:34 -0500

    se agregó archivos de vistas para reclamaciones

--- src/routes/complaintsBook.routes.js
+++ src/routes/complaintsBook.routes.js
@@ -4,5 +4,13 @@ const router = Router();
 router.get("/", (req, res)=>{
     res.render("links/complaintsBook");
 });
-
+router.get("/documentosIdentidad", (req, res)=>{
+    res.render("links/complaintsBook2");
+})
+router.get("/datosPersonales", (req, res)=>{
+    res.render("links/complaintsBook3");
+})
+router.get("/detalles", (req, res)=>{
+    res.render("links/complaintsBook4");
+})
 module.exports = router;
