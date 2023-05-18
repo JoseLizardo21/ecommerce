bf72da2f582b852dc18e8514610af76728c2b4ea
José Lizardo <jose.212002lizardo@gmail.com>
2023-05-17 21:38:07 -0500

    se añadieron archivos

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ app.set("view engine", ".hbs");
 //Routes
 app.use("/signUp",require("./routes/login.routes.js"));
 app.use("/contactanos", require("./routes/contactUs.routes.js"));
+app.use("/reservarCitas", require("./routes/appointments.routes.js"));
+app.use("/packs", require("./routes/packs.routes.js"));
+
 
 //static files
 app.use(express.static(path.join(__dirname, "public")))
