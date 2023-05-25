754a443efed49e35331adfd622fcdc3bc7e15692
Jose Lizardo <jose.212002lizardo@gmail.com>
2023-05-25 11:32:40 -0500

    se agregó nuevas ventanas para la compra de servicios

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -5,4 +5,16 @@ router.get("/", (req, res)=>{
     res.render("links/services");
 });
 
+router.get("/detalles", (req, res)=>{
+    res.render("links/servicesDetails");
+});
+
+router.get("/resumenCompra", (req, res)=>{
+    res.render("links/resumenCompra");
+});
+
+router.get("/datosCompra", (req, res)=>{
+    res.render("links/datosParaCompra");
+})
+
 module.exports = router;
